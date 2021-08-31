b85760605571a703eda43f5a5e01e1b8aa1c4f46
aleespa <aleespazx@gmail.com>
2021-08-31 21:01:38 +0100

    Addition of simulation functions

--- tools_ko/ko_estimation.py
+++ tools_ko/ko_estimation.py
@@ -1,3 +1,4 @@
+#tools_ko/ko_estimation
 import numpy as np
 import scipy as sp
 import matplotlib.pylab as plt
@@ -10,8 +11,158 @@ from scipy.spatial import KDTree
 
 class Koopman_estimation:
     def __init__(self,X):
+        """
+        To create the estimation object you only requiere
+        the dataset as numpy array shaped as (observartions,dimension)
+        """
         self.X = X #Dataset
         self.n = np.shape(self.X)[0] #Number of observations
         self.m = np.shape(self.X)[1] #Number of dimensions
+        print(f'Dataset with {self.n} observations and dimension {self.m}')
         
+    def density_estimation(self,epsilon_0= None):
+        """
+        Estimates the density given the data 
+
+        :param X: an nxm array 
+
+        :returns rho: a n dimensional function 
+        """
+        self.p_est = sp.stats.gaussian_kde((self.X).T,bw_method =epsilon_0)
+
+    def Manifold_volume_kernel(self,alternate_Norm,alternate_epsilon):
+        """
+        Estimation of the volume of the manifold
+        from a kernel and the bandwidth
+
+        :param Norm: an nxn numpy array
+        :param epsion: a float
+
+        :return T: a float
+        """
+        T =  (1/(self.n)**2)*np.exp(alternate_Norm/ alternate_epsilon).sum()
+        return(T)
+
+    def bandwidth_search(self,h = 1e-6,K=50,plot=False):
+        """
+        Computes the optimal bandwidth for the kernel
+        approximation given the data X and also computes
+        the intrisic dimension of the data 
+
+        :param X: an Nxn numpy array
+        :param h: perturbation parameter for the derivative
+
+        :return epsilon: a float
+        :return d: a float 
+        """
+        
+        alternate_Norm = -distance.squareform(distance.pdist(self.X, 'sqeuclidean'))/(4)
+        T = lambda alternate_epsilon: self.Manifold_volume_kernel(alternate_Norm,alternate_epsilon)
+        a = np.zeros(K)
+
+        for i,e in enumerate(2**np.linspace(-20,2,K)): 
+            a[i] = (log(T(e*2)) - log(T(e))) / (log(e*2) - log(e))
+
+        self.epsilon = 2 **np.linspace(-20,2,K)[np.argmax(a)]
+
+        self.d = 2 * (log(T(h+self.epsilon)) - log(T(self.epsilon))) / (log(self.epsilon+h) - log(self.epsilon))
+        
+        print(f"epsilon = {self.epsilon:.2e}")
+        print(f"d = {self.d:.2f}")
+
+        if plot:
+            fig,axs = plt.subplots(1,2,figsize=(12,3),dpi=100)
+
+            axs[0].plot(2**np.linspace(-20,2,K),a,zorder=1,color='#322671')
+            axs[0].scatter(self.epsilon,max(a),color='red',zorder=2)
+
+            axs[0].set_xlabel('$\epsilon$')
+            axs[0].set_ylabel('Power Law')
+            axs[0].set_xscale('log',base=2)
+
+            axs[1].plot(2**np.linspace(-20,2,K), [T(2**l) for l in np.linspace(-20,2)],zorder=1,color='#322671')
+            axs[1].scatter(self.epsilon,T(self.epsilon),s=30,color='red',zorder=2)
+            axs[1].set_xlabel('$\epsilon$')
+            axs[1].set_ylabel('$T(\epsilon)$')
+            axs[1].set_xscale('log',base=2)
+            plt.show()
+
+    def kernel_matrix(self,epsilon_0 = None):
+        """
+        Computes the variable bandwith 
+        kernel matrix given a set of observations X
+
+        :param X: an Nxn numpy array
+        """
+        self.rho = lambda x: self.p_est(x)**(-0.5)
+        self.Rho = self.rho((self.X).T).reshape(1,-1)
+        self.Norm = -distance.squareform(distance.pdist(self.X, 'sqeuclidean'))/(4 *((self.Rho).T @ (self.Rho)) * self.epsilon)
+        self.K_e = np.exp(self.Norm)
+
+    def  KNPGenerator(self,M=500,plot=False,return_extra=False,epsilon_0 = None,epsilon=None,d=None):
+        """
+        Computes eigenvalues and eigenvectors 
+        of the infinitesimal generator using the 
+        nonparametric approximation using kernels
+
+        :param X: an nxm array
+        :param M: a integer the number of eigenvectors to compute
     
+        :return l: set of M eigenvalues
+        :return phi: matrix of columns of eigenvectors
+        """
+
+        M = min(M,self.n)
+
+        self.alpha = -self.d/4
+
+        q_e = ((self.K_e).sum(axis=1) / ((self.Rho) ** (self.d))).reshape(-1,1)
+
+        self.K_e_a = self.K_e/((q_e**(self.alpha))@ (q_e** (self.alpha)).T)
+
+        I = np.identity(self.n)
+        q_e_a = (self.K_e_a).sum(axis=1)
+        
+        Dii = np.diag(q_e_a)
+        Pii = np.diag((self.Rho)[0,:])
+        Sii = Pii @ np.sqrt(Dii)
+        S_1 = np.diag(1/np.diag(Sii))
+        P_2 = np.diag(np.diag(Pii)**-2)
+        D_1 = np.diag(np.diag(Dii)**-1)
+
+        self.L_hat = (1/self.epsilon) * (S_1 @ (self.K_e_a) @ S_1 - P_2)
+        self.L_e =   (1/self.epsilon) * P_2 @ (D_1 @ (self.K_e_a) - I)
+
+        self.l, self.U = sp.linalg.eigh(self.L_hat,subset_by_index =(self.n-M,self.n-1),turbo=False)
+        self.l = self.l[::-1]
+        self.U = self.U[:,::-1]
+
+        self.phi = S_1 @ self.U
+        self.phi = self.phi / np.linalg.norm(self.phi,axis=0) * np.sqrt(self.n)    
+        self.U = self.U / np.linalg.norm(self.U,axis=0) * np.sqrt(self.n)
+
+    def diffusion_estimation(self,Xt,dt):
+        """
+        sd
+        """
+        n = len(Xt)
+        St = np.sum(Xt - Xt.mean(),axis=1)
+        C_tau=np.zeros(n)
+        C0 = (1/n) * np.sum(St**2)
+
+        for j in range(1,n):
+            if 1/(n-j)*sum(St[:n-j]*St[j:]) > 0:
+                C_tau[j] = 1/(n-j)*sum(St[:n-j]*St[j:])
+            else:
+                break
+
+        C_tau = C_tau[C_tau>0]
+        pv = len(C_tau)
+
+        Tc = simps(x=np.linspace(0,n*dt,n)[:pv],y= C_tau / C0)
+
+        S = np.sum(self.X - (self.X).mean(),axis=1)
+        s1 = np.sum((1/self.l) * (S.T@(self.phi))**2)
+        s2 = np.sum((S.T@(self.phi))**2)
+        self.D = - (1/Tc)* (s1/s2)
+        print(f'Diffusion  = {self.D}')
---
+++ tools_ko/simulations.py
@@ -0,0 +1,90 @@
+#tools_ko/simulations
+import numpy as np
+
+def OUSimulation(x0, n,dt,D):
+    """
+    Function to create the trayectory of a OU process
+    with difussion paramter D 
+
+    :param x0: Intial position
+    :param n:  Number of steps
+    "param dt: Step size
+    :param D:  Difussion parameter
+
+    :return X: Trajectory
+    """
+    X = np.zeros((n,1))
+    noise = np.random.normal(loc=0,scale=np.sqrt(dt),size=(n,1))
+    for i in range(1,n):
+        x = X[i-1,:]
+        X[i,:] = x - x*dt +np.sqrt(2*D) * noise[i,:]
+    return(X)
+
+
+def DWSimulation(x0, n,dt,D):
+    """
+    Function to create the trayectory of a OU process
+    with difussion paramter D 
+
+    :param x0: Intial position
+    :param n:  Number of steps
+    "param dt: Step size
+    :param D:  Difussion parameter
+
+    :return X: Trajectory
+    """
+    DV = lambda x:  5*x**5 - 10*x**3
+    t = np.linspace(x0,dt*n,n)
+    X = np.zeros((n,1))
+    noise = np.random.normal(loc=0,scale=np.sqrt(dt),size=(n,1))
+    for i in range(1,n):
+        x = X[i-1,:]
+        X[i,:] = x -DV(x)*dt +np.sqrt(2*D) * noise[i,:]
+    return(X)
+
+def OUSimulation2d(n=100000,dt = 0.0001,D=1):
+    """
+    Function to create the trayectory of a 2d OU process
+    with difussion paramter D starting at (0,0)
+
+    :param n:  Number of steps
+    :param dt: Step size
+    :param D:  Difussion parameter
+
+    :return Z: Trajectory
+    """
+    W = np.random.normal(loc=0,scale=np.sqrt(dt),size=(n,2))
+    Z = np.zeros((n,2))
+    Z[0,...] = 0
+    for i in range(1,n):
+        Z_1 = Z[i-1,...]
+        Z[i,...] = Z_1 - Z_1*dt + np.sqrt(2*D) * W[i,:]
+    return(Z)
+
+
+
+def QWSimulation2d(n=100000,dt = 0.0001,D=1):
+    """
+    Function to create the trayectory of a quadruple well
+    processwith difussion paramter D starting at (0,0)
+
+    :param n:  Number of steps
+    :param dt: Step size
+    :param D:  Difussion parameter
+
+    :return Z: Trajectory
+    """
+    def dV(X,k=4):
+        r = np.linalg.norm(X,axis=-1)
+        phi = np.arctan2(X[...,1],X[...,0]) 
+        dx1 = np.cos(phi) * (20*(r-1) - 1/r**2) - (1/r)*np.sin(phi)*(-k*np.sin(k*phi) +0.5*((1/np.cos(0.5 *phi))*np.tan(0.5 *phi)))
+        dx2 = np.sin(phi) * (20*(r-1) - 1/r**2) + (1/r)*np.cos(phi)*(-k*np.sin(k*phi) +0.5*((1/np.cos(0.5 *phi))*np.tan(0.5 *phi)))
+        return(np.array([dx1,dx2]))
+
+    W = np.random.normal(loc=0,scale=np.sqrt(dt),size=(n,2))
+    Z = np.zeros((n,2))
+    Z[0,...] = 1
+    for i in range(1,n):
+        Z_1 = Z[i-1,...]
+        Z[i,...] = Z_1 - dV(Z_1)*dt + np.sqrt(2*D) * W[i,:]
+    return(Z)
